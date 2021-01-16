13582a42076e3ed361d310ebf14bc41579dfc22d
Jakeehmann42 <Jakeehmann42@gmail.com>
2021-01-16 13:58:06 -0700

    Detailed the Navbar, added an About page when we click on the website title, and commented out some logic that we may need idk?

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,35 @@
-import { Link } from 'react-router-dom';
-import useAuth from '../hooks/auth';
-
 const Navbar = () => {
-    const { isLoggedIn, logout, getProfile } = useAuth();
+    // const { isLoggedIn, logout, getProfile } = useAuth();
     return (
         <div>
-            <h3>Navbar</h3>
+            <nav class="bg-indigo-700 shadow-lg">
+                <div class="container mx-auto">
+                    <div class="sm:flex justify-center">
+                        <a href="/About" class="text-white text-3xl font-bold p-3">LoL stack</a>
+                        <ul class="text-gray-400 sm:self-center text-xl border-t sm:border-none">
+                            
+                            <li class="sm:inline-block">
+                                <a href="/Home" class="p-3 hover: text-white">Home</a>
+                            </li>
+                            
+                            <li class="sm:inline-block">
+                                <a href="/Summoner" class="p-3 hover: text-white">Summoner</a>
+                            </li>
+                            
+                            <li class="sm:inline-block">
+                                <a href="/Signup" class="p-3 hover: text-white">Signup</a>
+                            </li>
+                            
+                            <li class="sm:inline-block">
+                                <a href="/Login" class="p-3 hover: text-white">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+            {/* <h3>Navbar</h3>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to='/summoners'>Summoner</Link></li>
@@ -20,8 +44,8 @@ const Navbar = () => {
                         <li><Link to="/login">Login</Link></li>
                     </>
                 }
-                
-            </ul>
+
+            </ul> */}
         </div>
     );
 };
