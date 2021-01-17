5f82cd9d8c38b5fb88688ff2cc0131439b664894
alhoff <andrew.hoff14@yahoo.com>
2021-01-17 10:41:36 -0700

    working on calling ddragon api for champ info

--- client/src/pages/Summoner.js
+++ client/src/pages/Summoner.js
@@ -5,14 +5,31 @@ import useAuth from '../hooks/auth';
 const Summoner = function () {
     const { getProfile } = useAuth();
     const [masteries, setMasteries] = useState([]);
+    const [champions, setChampions] = useState([]);
+    const [, setMergedChamps] = useState([]);
     useEffect(() => {
         fetchMasteries();
+        fetchChampions();
+        mergeChampions();
     }, []);
 
     async function fetchMasteries() {
         const { data } = await axios.get('/api/riot/masteries');
         setMasteries(data);
         console.log(data);
+    }
+
+    async function fetchChampions() {
+        const { data } = await axios.get('/api/riot/champions');
+        setChampions(data);
+        console.log(data);
+    }
+
+    function mergeChampions() {
+        setMergedChamps(masteries.map(t1 => ({ ...t1, ...champions.find(t2 => t2.data.key === t1.championId) })));
+        console.log('--------------------------------');
+        console.log(masteries.map(t1 => ({ ...t1, ...champions.find(t2 => t2.data.key === t1.championId) })));
+        console.log('--------------------------------');
     }
 
     // const handleSubmit = event => {
@@ -28,7 +45,14 @@ const Summoner = function () {
                 {masteries.map(mastery => {
                     return (
                         <li key={mastery.championId}>
-                            {JSON.stringify(mastery)}
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..."></img>
+                                <div className="card-body">
+                                    <h5 className="card-title">{mastery.championId}</h5>
+                                    <h6 className="card-text">Mastery level: {mastery.championLevel}</h6>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
                         </li>
                     );
                 })}
--- controllers/riotController.js
+++ controllers/riotController.js
@@ -18,4 +18,16 @@ router.get('/masteries', isAuthenticated, async function (req, res) {
 
 });
 
+router.get('/champions', isAuthenticated, async function (req, res) {
+    // we can pass in things in the query of a REST call!
+    // console.log(req.user)
+    const data = await axios({
+        method: 'get',
+        url: 'http://ddragon.leagueoflegends.com/cdn/11.1.1/data/en_US/champion.json'
+    });
+
+    res.json(data.data);
+
+});
+
 module.exports = router;
