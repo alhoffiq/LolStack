b2dec6bd8196df383d04bff91edc16dfd3999a1e
alhoff <andrew.hoff14@yahoo.com>
2021-01-19 21:08:43 -0700

    Merged champion data with user master data. Summoner page now displays user champions in order of highest mastery withalong with some data

--- client/src/pages/Summoner.js
+++ client/src/pages/Summoner.js
@@ -5,12 +5,10 @@ import useAuth from '../hooks/auth';
 const Summoner = function () {
     const { getProfile } = useAuth();
     const [masteries, setMasteries] = useState([]);
-    const [champions, setChampions] = useState([]);
-    const [, setMergedChamps] = useState([]);
+    const [, setChampions] = useState([]);
     useEffect(() => {
         fetchMasteries();
         fetchChampions();
-        mergeChampions();
     }, []);
 
     async function fetchMasteries() {
@@ -25,18 +23,11 @@ const Summoner = function () {
         console.log(data);
     }
 
-    function mergeChampions() {
-        setMergedChamps(masteries.map(t1 => ({ ...t1, ...champions.find(t2 => t2.data.key === t1.championId) })));
-        console.log('--------------------------------');
-        console.log(masteries.map(t1 => ({ ...t1, ...champions.find(t2 => t2.data.key === t1.championId) })));
-        console.log('--------------------------------');
-    }
-
     // const handleSubmit = event => {
     //     event.preventDefault();
     //     submitSummoner();
     // };
-
+    // http://ddragon.leagueoflegends.com/cdn/11.1.1/img/champion/
     return (
         <div>
             <h2>Summoner</h2>
@@ -46,11 +37,12 @@ const Summoner = function () {
                     return (
                         <li key={mastery.championId}>
                             <div className="card">
-                                <img src="..." className="card-img-top" alt="..."></img>
+                                <img src={`http://ddragon.leagueoflegends.com/cdn/11.1.1/img/champion/${mastery.champion.image.full}`} className="card-img-top" alt="..."></img>
                                 <div className="card-body">
-                                    <h5 className="card-title">{mastery.championId}</h5>
-                                    <h6 className="card-text">Mastery level: {mastery.championLevel}</h6>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h2 className="card-title">{mastery.champion.name}</h2>
+                                    <h3 className="card-text">Mastery level: {mastery.championLevel}</h3>
+                                    <h4 className="card-text">Mastery points: {mastery.championPoints}</h4>
+                                    <p className="card-text">{`Earned chest: ${mastery.chestGranted}`}</p>
                                 </div>
                             </div>
                         </li>
--- controllers/riotController.js
+++ controllers/riotController.js
@@ -1,4 +1,3 @@
-const db = require('../models');
 const router = require('express').Router();
 const isAuthenticated = require('../utils/middleware').isAuthenticated;
 const axios = require('axios');
@@ -9,12 +8,29 @@ const riotApi = process.env.RIOTKEY;
 router.get('/masteries', isAuthenticated, async function (req, res) {
     // we can pass in things in the query of a REST call!
     // console.log(req.user)
-    const data = await axios({
+    const { data: masteryData } = await axios({
         method: 'get',
         url: `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${req.user.summoner.id}?api_key=${riotApi}`
     });
+    const { data: championJson } = await axios({
+        method: 'get',
+        url: 'http://ddragon.leagueoflegends.com/cdn/11.1.1/data/en_US/champion.json'
+    });
 
-    res.json(data.data);
+    const champions = championJson.data;
+    const championMap = new Map();
+
+    for (const championName in champions) {
+        const champion = champions[championName];
+        championMap.set(champion.key, champion);
+    }
+    const masteryWithChampionData = masteryData.map(mastery => {
+        const id = mastery.championId;
+        const champion = championMap.get(`${id}`);
+        return { ...mastery, champion: champion };
+    });
+
+    res.json(masteryWithChampionData);
 
 });
 
