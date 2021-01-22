ac33effdf2c74a54bb65e176f44ac763fe5801f1
alhoff <andrew.hoff14@yahoo.com>
2021-01-21 20:10:48 -0700

    Added footer, updated Readme

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <span className="text-muted">Footer</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
+
