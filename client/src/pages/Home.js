295b129ef57a9b02af9ef13f9e22b4393b4a0c3e
Kristopher Kaae <kskaae@gmail.com>
2021-01-26 21:38:36 -0700

    HomePagethings

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,19 @@
 function Home() {
 
     return (
-        <div>
-            <h2>
-                Home Page
-            </h2>
-            <p>
-                Welcome Home
-            </p>
-        </div>
+        <body>
+            <h3 className="text-center"> Welcome to LOL Stack</h3>
+            <div className="input-group mb-3"></div>
+            <div className="input-group-prepend">
+            </div>
+            <div className="card text-white bg-dark mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">About Us</h5>
+                    <p className="card-text">League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)League of Legends is the second best MMO on the market (Next to World of Warcraft)</p>
+                </div>
+            </div>
+        </body>
     );
 }
-
 export default Home;
 
